85e5d8e7ff38861437bfb46f69b40187174e105d
Yuriy Oliferchuk <dbzero86@gmail.com>
2022-04-23 01:42:31 +0300

    session

--- server.js
+++ server.js
@@ -41,11 +41,9 @@ app.use(session({
     saveUninitialized: false,
     secret: "XXXX",
     cookie: {
-      path: '/',
-      httpOnly: true,
       maxAge: 1000*60*60*2,
-      // sameSite: true,
-      // secure: false,
+      sameSite: true,
+      secure: false,
     }
 }))
   
