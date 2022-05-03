b799b08a0ef4eb388b8e2283bdbad98177698e84
Yuriy Oliferchuk <dbzero86@gmail.com>
2022-05-03 17:28:14 +0300

    herocu test

--- routers/1_0.js
+++ routers/1_0.js
@@ -32,7 +32,7 @@ api1_0.get("/:lang/info", async (req, res) => {
   if (!result.rows[0]) {
     return res.status(400).json({ statusCode: 1, message: "Something wrong" });
   } else {
-    const myPhoto = req.protocol + '://' + req.hostname + '/pic/photo.jpg'
+    const myPhoto = "https://" + req.get("host") + "/pic/photo.jpg";
     res.json({ ...result.rows[0], photoUrl: myPhoto, statusCode: 0 });
   }
 });
