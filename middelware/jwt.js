611e8e05525a1888cf7bbf5d4a3c881a29af7889
Yuriy Oliferchuk <dbzero86@gmail.com>
2022-05-03 17:03:59 +0300

    herocu test

--- middelware/jwt.js
+++ middelware/jwt.js
@@ -57,7 +57,7 @@ const jwtAdminTokenCheckMiddelware = async (req, res, next) => {
 const jwtPassportMiddelware = (req, res, next) => {
   passport.authenticate("jwt", { session: false }, (err, user, info) => {
     
-    try {
+    // try {
       let token = req.headers.authorization.split(" ")[1];
       const secret = process.env.ACCESS_SECRET || "XXX";
           console.log("\n " + token);  
@@ -76,11 +76,11 @@ const jwtPassportMiddelware = (req, res, next) => {
           .json({ message: "Token from other user", statusCode: 1 });
       }
       next();
-    } catch (e) {
-      return res
-        .status(401)
-        .json({ message: "User not authorized", statusCode: 1 });
-    }
+    // } catch (e) {
+    //   return res
+    //     .status(401)
+    //     .json({ message: "User not authorized", statusCode: 1 });
+    // }
   })(req, res, next);
 };
 
